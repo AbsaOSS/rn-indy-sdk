3aabc19788758b32c001e3856784e58b3466a74d
Jakub Koci <jakub.koci@gmail.com>
2021-01-05 14:52:36 +0100

    Replace wget with curl

--- scripts/download-libindy.sh
+++ scripts/download-libindy.sh
@@ -8,11 +8,11 @@ mkdir -p android/app/src/main/jniLibs/arm64-v8a
 
 mkdir -p tmp
 pushd tmp
-  wget https://repo.sovrin.org/android/libindy/stable/${LIBINDY_VERSION}/libindy_android_arm64_${LIBINDY_VERSION}.zip
+  curl https://repo.sovrin.org/android/libindy/stable/${LIBINDY_VERSION}/libindy_android_arm64_${LIBINDY_VERSION}.zip --output libindy_android_arm64_${LIBINDY_VERSION}.zip
   unzip -a libindy_android_arm64_${LIBINDY_VERSION}.zip
   cp libindy_arm64/lib/libindy.so ../android/app/src/main/jniLibs/arm64-v8a
 
-  wget https://github.com/java-native-access/jna/raw/5.5.0/lib/native/android-${JNA_ARCH}.jar
+  curl https://github.com/java-native-access/jna/raw/5.5.0/lib/native/android-${JNA_ARCH}.jar -L --output android-${JNA_ARCH}.jar
   jar xf android-${JNA_ARCH}.jar libjnidispatch.so
   cp libjnidispatch.so ../android/app/src/main/jniLibs/arm64-v8a
 popd
